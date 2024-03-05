5ad40fe01c87aa212b168bc3b5e4a482e5a5dd7a
AMorohin <AMorohin@yandex.ru>
2024-03-05 21:06:55 +0300

    Переезд на редакс

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,53 @@
+import { useState } from "react";
+import './Main.css';
+
+
 function Main() {
+    const [todos, setTodos] = useState([]);
+    const [text, setText] = useState('');
+
+    const addTodo = () => {
+        if (text.trim().length) {
+            setTodos([
+                ...todos,
+                {
+                    id: new Date().toISOString(),
+                    text,
+                    completed: false,
+                }
+            ])
+        }
+    }
+
+    const toggleTodoComplete = (todoId) => {
+        setTodos(
+            todos.map(todo => {
+                if (todo.id !== todoId) return todo;
+
+                return {
+                    ...todo,
+                    completed: !todo.completed,
+                }
+            })
+        )
+    }
+
+    const removeTodo = (todoId) => {
+        setTodos(todos.filter(todo => todo.id !== todoId))
+    }
+
     return (
         <section className="main">
-            <h1 className="main-text">Главная</h1>
+            <label className="main__label">
+                <input className="main__input" type="text" value={text} onChange={(e) => setText(e.target.value)} />
+                <button className="main__input-buton" onClick={addTodo}>Добавить</button>
+            </label>
+            <ul className="main__list">
+                {todos.map(todo => <li className="main__list-item" key={todo.id}>
+                    <input type="checkbox" name="todo-check" className="main__list-item-checkbox" checked={todo.completed} onChange={() => toggleTodoComplete(todo.id)}/>
+                    <span className="main__list-item-text">{todo.text}</span>
+                    <button className="main__list-item-delete-button" onClick={() => removeTodo(todo.id)}>Удалить</button>
+                </li>)}</ul>
         </section>
     );
 }
